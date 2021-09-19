9ec33e6d0fece2555a87e90ec9299b654df31246
Susmita Kunde <susmitakunde@gmail.com>
2021-09-19 13:11:49 +0530

    functionalities in Job and Blog added

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -122,7 +122,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+STATICFILES_DIRS = [
+BASE_DIR / 'portfolio/static/'
+]
+STATIC_ROOT = BASE_DIR / 'static'
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
